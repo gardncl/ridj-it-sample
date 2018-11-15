847e37cd33e3034ebcafd0fc2b9fae0829b2b370
gardncl <gardncl@gmail.com>
2018-11-14 21:28:29 -0500

    seed database

---
+++ src/main/resources/db/migration/V3__seed_database.sql
@@ -0,0 +1,13 @@
+insert into locations
+    (name, address, city, state)
+values
+    ('Mt Moriah', '69 Roughneck Road', 'White Mountains', 'NH'),
+    ('Mt Washington', '4 Presidential Traverse Lane', 'White Mountains', 'NH');
+
+insert into trips
+    (name, description, date, location)
+values
+    ('Moriah Trip', 'Lets get out into wilderness', '11-17-2018', 1),
+    ('Winter Washington Trip', 'Lets hike this thing the middle of winter', '02-14-2019', 2),
+    ('Summer Washington Trip', 'Lets hike this thing the middle of summer', '06-03-2019', 2);
+
